6f4528967b52fd76e57c8bc6538037425bcb8457
Febri Hidayan <febrihidayan20@gmail.com>
2020-09-23 19:43:47 +0700

    init commit

---
+++ nuxt.config.js
@@ -0,0 +1,19 @@
+export default {
+
+    mode: 'static',
+
+    head: {
+        title: process.env.APP_NAME,
+        titleTemplate: '%s - Pusat Bantuan Dari Sekolah Program'
+    },
+
+    env: {
+        appName: process.env.APP_NAME
+    },
+
+    modules: [
+        '@nuxt/components',
+        '@nuxt/content'
+    ]
+
+}
